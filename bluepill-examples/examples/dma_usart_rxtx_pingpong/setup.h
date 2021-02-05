025d6ea4ceceec1539d0b07e67870fbfc73cae4a
iminb2b <65203827+iminb2b@users.noreply.github.com>
2021-02-05 01:33:26 -0500

    add Buzzer

---
+++ bluepill-examples/examples/dma_usart_rxtx_pingpong/setup.h
@@ -0,0 +1,14 @@
+
+#ifndef SETUP_H
+#define SETUP_H
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/scb.h>
+#include <libopencm3/stm32/dma.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+void setup();
+
+#endif
